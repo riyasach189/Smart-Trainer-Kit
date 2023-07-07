61f01d66be5d4d0da65926ebc4e4e5868ba8fc25
Surat Sathi Samanta <surat22517@iiitd.ac.in>
2023-07-07 12:57:56 +0530

    done

--- gate tester/gatetester.py
+++ gate tester/gatetester.py
@@ -2,6 +2,7 @@
 def gatetester(current_IC):
 #for not gate change this to whatever ndex is used for other ICs
     exec("import machine")
+    from ics import string_to_index
     current_IC = string_to_index(current_IC)
 
     gpio=[4,5,6,7,9,10,11,12,24,25,26,27,32,34]
@@ -121,10 +122,6 @@ def gpio_to_gate(num, ic):
         else:
             return num + 2
 
-    
 
-def string_to_index(ic):
-    ics = {"74LS04":0, "74LS08":1, "CD4001":2, "CD4011":3, "CD4070":4, "CD4071":5, "CD4077":6, "CD4081":7, "SN74LS157":8, "CD4013B":9, "74LS153":10}
-    return ics[ic]
         
 gatetester(0)
---
+++ gate tester/ics.py
@@ -0,0 +1,3 @@
+def string_to_index(ic):
+    ics = {"74LS04":0, "74LS08":1, "CD4001":2, "CD4011":3, "CD4070":4, "CD4071":5, "CD4077":6, "CD4081":7, "SN74LS157":8, "CD4013B":9, "74LS153":10}
+    return ics[ic]
