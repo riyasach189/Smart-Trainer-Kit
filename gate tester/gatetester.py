2f6574164e493fbc05ced1373ffdcf7e484866b8
Surat Sathi Samanta <surat22517@iiitd.ac.in>
2023-07-07 11:37:15 +0530

    aalas

--- gate tester/gatetester.py
+++ gate tester/gatetester.py
@@ -1,22 +1,108 @@
-current_IC=0  #for not gate change this to whatever ndex is used for other ICs
-import machine
-gpio=[4,5,6,7,9,10,11,12,24,25,26,27,32,34]
-pinoutlist=[['OUT','IN','OUT','IN','OUT','IN','OUT','OUT','IN','OUT','IN','OUT','IN','OUT']]
-truthtablelist=[[[1,0,1,0,1,0,0,0,1,0,1,0,1,0],[0,1,0,1,0,1,0,0,1,0,1,0,1,0]]]
-testcasecount = len(truthtablelist[current_IC])
+def gatetester(current_IC):
+#for not gate change this to whatever ndex is used for other ICs
+    import machine
 
-for testcasecount in range(testcasecountlimit):
-    testcaseerror = []
-    for i in range(len(pinoutlist)):
-        exec('dip_' + str(i) + '=machine.Pin(' + str(gpio[i]) + ', Pin.' + pinoutlist[0][i] + ')')
-        if pinoutlist[0][i] == 'OUT':
-            exec('dip_' + str(i + '.value(' + truthtablelist[0][testcasecount][i] + ')'))
+    gpio=[4,5,6,7,9,10,11,12,24,25,26,27,32,34]
+    pinoutlist=[
+        ['OUT','IN','OUT','IN','OUT','IN','OUT','OUT','IN','OUT','IN','OUT','IN','OUT'],  # 74LS04 not
+        ['OUT','OUT','IN','OUT','OUT','IN','OUT','OUT','IN','OUT','OUT','IN','OUT','OUT'], # 74LS08 and
+        ['OUT','OUT','IN','IN','OUT','OUT','OUT','OUT','OUT','OUT','IN','IN','OUT','OUT'], # CD4001 nor
+        ['OUT','OUT','IN','IN','OUT','OUT','OUT','OUT','OUT','OUT','IN','IN','OUT','OUT'], # CD4011 nand
+        ['OUT','OUT','IN','IN','OUT','OUT','OUT','OUT','OUT','OUT','IN','IN','OUT','OUT'], # CD4070 xor
+        ['OUT','OUT','IN','IN','OUT','OUT','OUT','OUT','OUT','OUT','IN','IN','OUT','OUT'], # CD4071 or
+        ['OUT','OUT','IN','IN','OUT','OUT','OUT','OUT','OUT','OUT','IN','IN','OUT','OUT'], # CD4077 xnor
+        ['OUT','OUT','IN','IN','OUT','OUT','OUT','OUT','OUT','OUT','IN','IN','OUT','OUT'], # CD4081 and
+        ['OUT','OUT','OUT','IN','OUT','OUT','IN','IN','OUT','OUT','IN','OUT','OUT','OUT'], # SN74LS157 mux
+        ]
+    truthtablelist=[
+        [
+            [1,0,1,0,1,0,0,0,0,1,0,1,0,1],                                                # 74LS04 not
+            [0,1,0,1,0,1,0,0,1,0,1,0,1,0]
+        ],
+        [
+            [1,1,1,1,1,1,0,0,1,1,1,1,1,1],                                                # 74LS08 and
+            [1,0,0,1,0,0,0,0,0,0,1,0,0,1],
+            [0,1,0,0,1,0,0,0,0,1,0,0,1,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        ],
+        [
+            [1,1,0,0,1,1,0,0,1,1,0,0,1,1],                                                # CD4001 nor
+            [1,0,0,0,0,1,0,0,1,0,0,0,0,1],
+            [0,1,0,0,1,0,0,0,0,1,0,0,1,0],
+            [0,0,1,1,0,0,0,0,0,0,1,1,0,0]
+        ],
+        [
+            [1,1,0,0,1,1,0,0,1,1,0,0,1,1],                                                # CD4011 nand
+            [1,0,1,1,0,1,0,0,1,0,1,1,0,1],
+            [0,1,1,1,1,0,0,0,0,1,1,1,1,0],
+            [0,0,1,1,0,0,0,0,0,0,1,1,0,0]
+        ],
+        [
+            [1,1,0,0,1,1,0,0,1,1,0,0,1,1],                                                # CD4070 xor
+            [1,0,1,1,0,1,0,0,1,0,1,1,0,1],
+            [0,1,1,1,1,0,0,0,0,1,1,1,1,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        ],
+        [
+            [1,1,1,1,1,1,0,0,1,1,1,1,1,1],                                                # CD4071 or
+            [1,0,1,1,0,1,0,0,1,0,1,1,0,1],
+            [0,1,1,1,1,0,0,0,0,1,1,1,1,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        ],
+        [
+            [1,1,1,1,1,1,0,0,1,1,1,1,1,1],                                                # CD4077 xnor
+            [1,0,0,0,0,1,0,0,1,0,0,0,0,1],
+            [0,1,0,0,1,0,0,0,0,1,0,0,1,0],
+            [0,0,1,1,0,0,0,0,0,0,1,1,0,0]
+        ],
+        [
+            [1,1,1,1,1,1,0,0,1,1,1,1,1,1],                                                # CD4081 and
+            [1,0,0,0,0,1,0,0,1,0,0,0,0,1],
+            [0,1,0,0,1,0,0,0,0,1,0,0,1,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        ],
+        [
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,1],                                                # SN74LS157 mux
+            [0,0,1,0,0,1,0,0,1,0,0,1,0,1],
+            [0,1,0,0,1,0,0,0,0,1,0,0,1,1],
+            [0,1,1,0,1,1,0,0,1,1,0,1,1,1],
+
+            [1,0,0,0,0,0,0,0,0,0,0,0,0,1],
+            [1,0,1,0,0,1,0,0,1,0,0,1,0,1],
+            [1,1,0,0,1,0,0,0,0,1,0,0,1,1],
+            [1,1,1,0,1,1,0,0,1,1,0,1,1,1],
+
+
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,1,1,0,1,1,1,1,0,1,1,0,0],
+            [0,1,0,0,1,0,0,0,0,1,0,0,1,0],
+            [0,1,1,1,1,1,1,1,1,1,1,1,1,0],
+
+            [1,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [1,0,1,0,0,1,0,0,1,0,0,1,0,0],
+            [1,1,0,1,1,0,1,1,0,1,1,0,1,0],
+            [1,1,1,1,1,1,1,1,1,1,1,1,1,0]
+        ],
+        [
             
-    for i in range(len(pinoutlist)):
-        if pinoutlist[0][i] == 'OUT':
-            continue
-        exec('pinval=dip_' + str(i) + 'value()')
-        if truthtablelist[0][testcasecount][i] == pinval:
-            continue
-        else:
-            testcaseerror.append(i)
+        ]
+    ]
+
+    testcasecountlimit = len(truthtablelist[current_IC])
+    pinval = None
+
+    for testcasecount in range(testcasecountlimit):
+        testcaseerror = []
+        for i in range(len(pinoutlist)):
+            exec('dip_' + str(i) + '=machine.Pin(' + str(gpio[i]) + ', Pin.' + pinoutlist[current_IC][i] + ')')
+            if pinoutlist[current_IC][i] == 'OUT':
+                exec('dip_' + str(i + '.value(' + truthtablelist[current_IC][testcasecount][i] + ')'))
+                
+        for i in range(len(pinoutlist)):
+            if pinoutlist[current_IC][i] == 'OUT':
+                continue
+            exec('pinval=dip_' + str(i) + 'value()')
+            if truthtablelist[current_IC][testcasecount][i] == pinval:
+                continue
+            else:
+                testcaseerror.append(i)
