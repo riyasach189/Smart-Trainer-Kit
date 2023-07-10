6e3f742b0333826ce85abe576714111014cf742d
Surat Sathi Samanta <62372847+kio42069@users.noreply.github.com>
2023-07-10 12:47:23 +0530

    Update gatetester.py

--- gate tester/gatetester.py
+++ gate tester/gatetester.py
@@ -1,8 +1,8 @@
+import machine
 
 def gatetester(current_IC):
-#for not gate change this to whatever ndex is used for other ICs
-    exec("import machine")
-    from ics import string_to_index
+#for not gate change this to whatever index is used for other ICs
+
     current_IC = string_to_index(current_IC)
 
     gpio=[4,5,6,7,9,10,11,12,24,25,26,27,32,34]
@@ -94,9 +94,9 @@ def gatetester(current_IC):
 
     for testcasecount in range(testcasecountlimit):
         for i in range(len(pinoutlist)):
-            exec('dip_' + str(i) + '=machine.Pin(' + str(gpio[i]) + ', Pin.' + pinoutlist[current_IC][i] + ')')
+            exec('dip_' + str(i) + '=machine.Pin(' + str(gpio[i]) + ', machine.Pin.' + pinoutlist[current_IC][i] + ')')
             if pinoutlist[current_IC][i] == 'OUT':
-                exec('dip_' + str(i + '.value(' + truthtablelist[current_IC][testcasecount][i] + ')'))
+                exec('dip_' + str(i) + '.value(' + str(truthtablelist[current_IC][testcasecount][i]) + ')')
                 
         for i in range(len(pinoutlist)):
             if pinoutlist[current_IC][i] == 'OUT':
@@ -107,7 +107,7 @@ def gatetester(current_IC):
             else:
                 testcaseerror.append(gpio_to_gate(i, current_IC))
 
-    return testcaseerror
+    return list(set(testcaseerror))
 
 
 def gpio_to_gate(num, ic):
@@ -122,6 +122,7 @@ def gpio_to_gate(num, ic):
         else:
             return num + 2
 
+def string_to_index(ic):
+    ics = {"74LS04":0, "74LS08":1, "CD4001":2, "CD4011":3, "CD4070":4, "CD4071":5, "CD4077":6, "CD4081":7, "SN74LS157":8, "CD4013B":9, "74LS153":10}
+    return ics[ic]
 
-        
-gatetester(0)
